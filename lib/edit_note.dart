b232c5e9785ceffec2ef9698e559a9514228ba02
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-19 20:10:54 +0100

    added

--- lib/edit_note.dart
+++ lib/edit_note.dart
@@ -10,12 +10,36 @@ class EditNote extends StatefulWidget {
 class _EditNoteState extends State<EditNote> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(
-        children: [
-          TextField()
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            const TextField(),
+            const Spacer(),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white10,
+                ),
+                child: const Text('Add',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const EditNote()),
+                  );
+                }
+            ),
+            const SizedBox(
+              height: 50,
+            )
+          ],
+        ),
       ),
     );
   }
